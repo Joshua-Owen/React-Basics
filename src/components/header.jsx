338dbda825246cf60beb610362bfe189ca6096fe
Xenii <joshua068@hotmail.co.uk>
2026-03-17 10:12:12 +0000

    adding styles

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,31 +1,30 @@
 
-const getTheYear = () => { //creates a funtion that will create a instance of the date class and then return the full year. This is the same as 
-                        //{const newDate = new Date(); return newDate.getFullYear(); but its done outside the component to return a single expression
+const getTheYear = () => { 
     const newDate = new Date();
     return newDate.getFullYear();
 }
 
-
-//const Header = () => (
-   // <>
-      // The date is {Date.now()} {/*this is an single expression as its returning 1 thing*/}
-        {/* The year is {const newDate = new Date(); return newDate.getFullYear();} this is all valid however this is not a single expression it is 2 
-                                                                                    1, the creation of the variable and 2, the return of said vairable
-                                                                                    this is why it is causing a in line error*/}
-
-      //    The year is {getTheYear()}  
-          {/*we can create a function that can do multiple calcs and then call the funstions instead to get past the multiple expressions issue*/}   
-     //     what is 5 + 5? {5+5}                                                                         
-   // </>
-//)
-
+/*
+makes classes for the styles that we add inline 
+as this is javascript we dont get auto-fill
+let styles = {
+    header:{
+        background: '#03a9f4'
+    },
+    logo:{
+        color: '#ffffff',
+        fontFamily: 'Anton',
+        textAlign: 'center'
+    }
+}
+*/
 const Header = () => {
-    return( /*we must return with () when using {} for the component */
+    return(
     <header>
-        <div>
+        <div className="logo">
             Logo
         </div>
-        <input/> {/* allows users to input strings*/}
+        <input/>
     </header>
 )}
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 
 import { createRoot } from 'react-dom/client';  
 import Header from './components/header';
+import './styles/styles.css'; //importing to main.jsx allow it to be used throughout the project
 
 const App = () => (
 
